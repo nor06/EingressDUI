f0a73055c25ef231c79debd30b78fd9296531ae6
nor06 <147531372+nor06@users.noreply.github.com>
2025-05-28 10:10:06 +0800

    reverted env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production : true,
     // api url
-        baseURL : 'http://192.168.254.105:3000/' 
+        // baseURL : 'http://104.43.100.222:3000/' 
         //apiurl from jairo api
-        //baseURL : 'https://jairoeingressapi.azurewebsites.net/' 
-        //baseURL : 'http://localhost:3000/'
+        // baseURL : 'https://jairoeingressapi.azurewebsites.net/' 
+        baseURL : 'http://192.168.42.72:3000/'
     } 
